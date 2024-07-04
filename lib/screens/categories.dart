fe9b89bb45dec6bf6fc9dd3056e493df8e81e322
bagaspt <bagas.saputra@students.paramadina.ac.id>
2024-07-04 20:56:41 +0700

    Add Step 12

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import '/data/dummy_data.dart';
+import '/models/meal.dart';
 import '/widgets/category_grid_item.dart';
 import '/screens/meals.dart';
 import '/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -17,6 +20,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     ); // Navigator.push(context, route)
